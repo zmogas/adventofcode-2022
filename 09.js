237a14f8a2960b83a25b7ba793d3a861a46e2e3e
Tomas Gudauskas <zmogas@gmail.com>
2022-12-11 22:27:59 +0200

    Solved day 9 part 2:
    * Answer1: 5683
    * Answer2: 2372
    * Rewrote common function to adjust to variable length of rope

--- 09.js
+++ 09.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 
-const day = "09e";
+const day = "09";
 
 const input = fs.readFileSync(`${day}.txt`, "utf-8");
 
@@ -11,52 +11,67 @@ grid["t0"] = [];
 grid["t0"]["t0"] = 1;
 
 let [minX, minY, maxX, maxY] = [0, 0, 0, 0];
-// Head position
-let [hx, hy] = [0, 0];
-// Tail position
-let [tx, ty] = [0, 0];
 
-const move = (deltax, deltay, diff) => {
-  hx += deltax;
-  hy += deltay;
-  const adjust = 0;
+const posX = [];
+const posY = [];
+
+let length = 2;
+for (i = 0; i < length; i++) {
+  posX[i] = 0;
+  posY[i] = 0;
+}
+
+const move = (deltax, deltay, position) => {
+  if (position === 0) {
+    posX[position] += deltax;
+    posY[position] += deltay;
+  }
 
   // Diagonal move
-  if (hx - tx > diff) {
-    tx += 1;
-    if (hy - ty > adjust) {
-      ty += 1;
-    } else if (hy - ty < adjust) {
-      ty -= 1;
+  if (posX[position] - posX[position + 1] > 1) {
+    posX[position + 1] += 1;
+    if (posY[position] - posY[position + 1] > 0) {
+      posY[position + 1] += 1;
+    } else if (posY[position] - posY[position + 1] < 0) {
+      posY[position + 1] -= 1;
     }
-  } else if (hx - tx < -1 * diff) {
-    tx -= 1;
-    if (hy - ty > adjust) {
-      ty += 1;
-    } else if (hy - ty < adjust) {
-      ty -= 1;
+  } else if (posX[position] - posX[position + 1] < -1) {
+    posX[position + 1] -= 1;
+    if (posY[position] - posY[position + 1] > 0) {
+      posY[position + 1] += 1;
+    } else if (posY[position] - posY[position + 1] < 0) {
+      posY[position + 1] -= 1;
     }
-  } else if (hy - ty > diff) {
-    ty += 1;
-    if (hx - tx > adjust) {
-      tx += 1;
-    } else if (hx - tx < adjust) {
-      tx -= 1;
+  } else if (posY[position] - posY[position + 1] > 1) {
+    posY[position + 1] += 1;
+    if (posX[position] - posX[position + 1] > 0) {
+      posX[position + 1] += 1;
+    } else if (posX[position] - posX[position + 1] < 0) {
+      posX[position + 1] -= 1;
     }
-  } else if (hy - ty < -1 * diff) {
-    ty -= 1;
-    if (hx - tx > adjust) {
-      tx += 1;
-    } else if (hx - tx < adjust) {
-      tx -= 1;
+  } else if (posY[position] - posY[position + 1] < -1) {
+    posY[position + 1] -= 1;
+    if (posX[position] - posX[position + 1] > 0) {
+      posX[position + 1] += 1;
+    } else if (posX[position] - posX[position + 1] < 0) {
+      posX[position + 1] -= 1;
     }
   }
 
-  // Mark grid as visited
-  if (grid["t" + tx] === undefined) {
-    grid["t" + tx] = [];
+  // console.log(
+  //   position,
+  //   posX[position],
+  //   posY[position],
+  //   posX[position + 1],
+  //   posY[position + 1]
+  // );
+  if (position === length - 2) {
+    // Mark grid as visited
+    if (grid["t" + posX[position + 1]] === undefined) {
+      grid["t" + posX[position + 1]] = [];
+    }
+    grid["t" + posX[position + 1]]["t" + posY[position + 1]] = 1;
   }
-  grid["t" + tx]["t" + ty] = 1;
 };
 
 lines.map((line) => {
@@ -65,32 +80,32 @@ lines.map((line) => {
   switch (direction) {
     case "R":
       for (i = 0; i < step; i++) {
-        move(1, 0, 1);
+        move(1, 0, 0);
       }
       break;
     case "L":
       for (i = 0; i < step; i++) {
-        move(-1, 0, 1);
+        move(-1, 0, 0);
       }
       break;
     case "D":
       for (i = 0; i < step; i++) {
-        move(0, 1, 1);
+        move(0, 1, 0);
       }
       break;
     case "U":
       for (i = 0; i < step; i++) {
-        move(0, -1, 1);
+        move(0, -1, 0);
       }
       break;
   }
-  minX = Math.min(minX, hx);
-  maxX = Math.max(maxX, hx);
-  minY = Math.min(minY, hy);
-  maxY = Math.max(maxY, hy);
+  // minX = Math.min(minX, hx);
+  // maxX = Math.max(maxX, hx);
+  // minY = Math.min(minY, hy);
+  // maxY = Math.max(maxY, hy);
 });
 
-console.log({ x: hx, y: hy, minX, maxX, minY, maxY });
+// console.log({ x: hx, y: hy, minX, maxX, minY, maxY });
 
 // console.log({ grid });
 let count = 0;
@@ -105,10 +120,13 @@ console.log("Answer1:", count);
 grid = [];
 grid["t0"] = [];
 grid["t0"]["t0"] = 1;
-// Head position
-[hx, hy] = [0, 0];
-// Tail position
-[tx, ty] = [0, 0];
+
+length = 10;
+for (i = 0; i < length; i++) {
+  posX[i] = 0;
+  posY[i] = 0;
+}
+
 [minX, minY, maxX, maxY] = [0, 0, 0, 0];
 
 lines.map((line) => {
@@ -117,38 +135,47 @@ lines.map((line) => {
   switch (direction) {
     case "R":
       for (i = 0; i < step; i++) {
-        move(1, 0, 9);
+        for (j = 0; j < length; j++) {
+          move(1, 0, j);
+        }
       }
       break;
     case "L":
       for (i = 0; i < step; i++) {
-        move(-1, 0, 9);
+        for (j = 0; j < length; j++) {
+          move(-1, 0, j);
+        }
       }
       break;
     case "D":
       for (i = 0; i < step; i++) {
-        move(0, 1, 9);
+        for (j = 0; j < length; j++) {
+          move(0, 1, j);
+        }
       }
       break;
     case "U":
       for (i = 0; i < step; i++) {
-        move(0, -1, 9);
+        for (j = 0; j < length; j++) {
+          move(0, -1, j);
+        }
       }
       break;
   }
-  minX = Math.min(minX, hx);
-  maxX = Math.max(maxX, hx);
-  minY = Math.min(minY, hy);
-  maxY = Math.max(maxY, hy);
+
+  minX = Math.min(minX, posX[length - 1]);
+  maxX = Math.max(maxX, posX[length - 1]);
+  minY = Math.min(minY, posY[length - 1]);
+  maxY = Math.max(maxY, posY[length - 1]);
 });
 
-for (y = minY; y <= maxY; y++) {
-  line = "";
-  for (x = minX; x <= maxX; x++) {
-    line += !!grid["t" + x] && !!grid["t" + x]["t" + y] ? "#" : ".";
-  }
-  console.log(line);
-}
+// for (y = minY; y <= maxY; y++) {
+//   line = "";
+//   for (x = minX; x <= maxX; x++) {
+//     line += !!grid["t" + x] && !!grid["t" + x]["t" + y] ? "#" : ".";
+//   }
+//   console.log(line);
+// }
 
 // console.log(grid);
 count = 0;
