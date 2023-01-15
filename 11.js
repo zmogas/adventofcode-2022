e5c17d6e88676ea2092b11e9e599b72fc6bc45d9
Tomas Gudauskas <zmogas@gmail.com>
2023-01-15 02:00:47 +0200

    Day 11 part not solved:
    * Added sort, that somehow lost previously
    * We get super big numbers, even BigInt is too small
    * Maybe not too small, but 1000 round calculation takes forever (500 in second, 600 in ~7 seconds)
    * Really need to find another way to keep levels manageable
    * EDIT: 1000 took ober 6 minutes and counts were already wrong.

--- 11.js
+++ 11.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 
-const day = "11";
+const day = "11d";
 
 const input = fs.readFileSync(`${day}.txt`, "utf-8");
 
@@ -17,7 +17,7 @@ monkeyText.map((monkey) => {
   const ifFalse = lines[5].split(/If false: throw to monkey (\d)/)[1];
 
   monkeys.push({
-    items: items.split(", "),
+    items: items.split(", ").map((a) => parseInt(a)),
     operation: operation.split(" "),
     divisible: parseInt(divisible),
     ifTrue: parseInt(ifTrue),
@@ -27,6 +27,29 @@ monkeyText.map((monkey) => {
 });
 
 console.log(monkeys);
+
+function deepCopy(obj) {
+  if (Object.prototype.toString.call(obj) === "[object Array]") {
+    var out = [],
+      i = 0,
+      len = obj.length;
+    for (; i < len; i++) {
+      out[i] = arguments.callee(obj[i]);
+    }
+    return out;
+  }
+  if (typeof obj === "object") {
+    var out = {},
+      i;
+    for (i in obj) {
+      out[i] = arguments.callee(obj[i]);
+    }
+    return out;
+  }
+  return obj;
+}
+
+const monkeys2 = deepCopy(monkeys);
 
 const inspect = (monkey) => {
   const { items, operation, divisible, ifTrue, ifFalse } = monkey;
@@ -62,8 +85,58 @@ for (round = 1; round <= maxRound; round++) {
 
 const counts = [];
 monkeys.map(({ count }) => counts.push(count));
+counts.sort((a, b) => b - a);
 
 const answer1 = counts[0] * counts[1];
+console.log({ counts });
 
 console.log("Answer1:", answer1);
 // 112815
+console.log();
+console.log();
+
+const inspect2 = (monkey) => {
+  const { items, operation, divisible, ifTrue, ifFalse } = monkey;
+  while (items.length > 0) {
+    monkey.count++;
+    let worry = BigInt(items.shift());
+
+    const unit = BigInt(
+      operation[1] === "old" ? worry : parseInt(operation[1])
+    );
+    if (operation[0] === "+") {
+      worry = BigInt(worry + unit);
+    } else {
+      worry = BigInt(worry * unit);
+    }
+
+    // worry = parseInt(worry / 3);
+
+    const multiplier = worry / BigInt(divisible);
+
+    // if (worry % BigInt(divisible) === 0) {
+    if (multiplier * BigInt(divisible) === worry) {
+      monkeys2[ifTrue].items.push(worry);
+    } else {
+      monkeys2[ifFalse].items.push(worry);
+    }
+  }
+};
+
+const maxRound2 = 1000; //10000;
+for (round = 1; round <= maxRound2; round++) {
+  for (i = 0; i < monkeys2.length; i++) {
+    inspect2(monkeys2[i]);
+  }
+}
+
+console.log(monkeys2);
+
+const counts2 = [];
+monkeys2.map(({ count }) => counts2.push(count));
+counts2.sort((a, b) => b - a);
+
+const answer2 = counts2[0] * counts2[1];
+
+console.log("Answer2:", answer2);
+// 17287536232 is too low
